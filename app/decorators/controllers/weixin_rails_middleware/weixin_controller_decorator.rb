8d5e17a3919a1021553cc3007b68a6f57ed471b8
Akira Panda <p.chenliang@gmail.com>
2014-09-24 23:11:55 +0800

    fix a bug

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -3,6 +3,8 @@
 # 2, @weixin_public_account: 如果配置了public_account_class选项,则会返回当前实例,否则返回nil.
 # 3, @keyword: 目前微信只有这三种情况存在关键字: 文本消息, 事件推送, 接收语音识别结果
 WeixinRailsMiddleware::WeixinController.class_eval do
+  include Rails.application.routes.url_helpers
+  
   def server_path
     "http://115.29.189.26"
   end
