9ca0fd10ac2ff4785cad221eaea24023f896540d
Akira Panda <p.chenliang@gmail.com>
2014-09-26 20:33:34 +0800

    add lesson basic view

--- app/controllers/admin/wx_lessons_controller.rb
+++ app/controllers/admin/wx_lessons_controller.rb
@@ -5,4 +5,27 @@ class  Admin::WxLessonsController < Admin::BaseController
       @lessons = @q.result(distinct: true)
       @lessons_grid = initialize_grid(@lessons,:per_page => 20)
     end
+    
+    def show
+      @lesson = WxLesson.find(params[:id])
+    end
+    
+    def new
+      @lesson = WxLesson.new
+    end
+    
+    
+    def create
+      @lesson = WxLesson.new(wx_lesson_params)
+      if @lesson.save
+        redirect_to [:admin,@lesson],:notice=>"新建课程主题成功!"
+      else
+        render 'new'
+      end
+    end
+    
+    private
+    def wx_lesson_params
+      params.require(:wx_lesson).permit!
+    end
 end
--- app/models/wx_lesson.rb
+++ app/models/wx_lesson.rb
@@ -1,4 +1,6 @@
 class WxLesson < ActiveRecord::Base
   has_many :wx_chapters
+  mount_uploader :cover, ArticleCoverUploaderUploader
+  
 end
 
