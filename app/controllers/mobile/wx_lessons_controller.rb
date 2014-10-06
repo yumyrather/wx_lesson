6cbe90aa9c3ce7a0fe63b8b1657b555a26669ed2
Akira Panda <p.chenliang@gmail.com>
2014-10-06 17:31:14 +0800

    rename model

--- app/controllers/mobile/wx_lessons_controller.rb
+++ app/controllers/mobile/wx_lessons_controller.rb
@@ -1,6 +1,11 @@
 class  Mobile::WxLessonsController < Mobile::BaseController  
     def show
       @lesson = WxLesson.find(params[:id])
-      @chapters = @lesson.wx_chapters.where("no is not null").order("no asc")
+      if current_wx_user
+        @study_record = @lesson.wx_lesson_user_records.find_by_wx_user_id(current_wx_user.id)
+        @chapters = @lesson.wx_chapters.where("no > 0 and no is not null and no <= ?",@study_record.now_chatpter).order("no asc")
+      else
+        @chapters = []  
+      end
     end
 end
