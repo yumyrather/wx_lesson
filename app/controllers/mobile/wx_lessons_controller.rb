16ea806c7a965b243c5d42e6946b4844a825df9e
Akira Panda <p.chenliang@gmail.com>
2014-10-10 11:28:29 +0800

    Add

--- app/controllers/mobile/wx_lessons_controller.rb
+++ app/controllers/mobile/wx_lessons_controller.rb
@@ -3,7 +3,7 @@ class  Mobile::WxLessonsController < Mobile::BaseController
       @lesson = WxLesson.find(params[:id])
       if current_wx_user
         @study_record = @lesson.wx_lesson_user_records.find_by_wx_user_id(current_wx_user.id)
-        now_chapter = @study_record.nil? 0 : @study_record.now_chatpter
+        now_chapter = @study_record.nil? ? 0 : @study_record.now_chatpter
         @chapters = @lesson.wx_chapters.where("no > 0 and no is not null and no <= ?",now_chapter).order("no asc")
       else
         @chapters = []  
