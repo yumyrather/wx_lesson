0c2e6ea56eaba09d9e095fd40dc1802f8f556739
Akira Panda <p.chenliang@gmail.com>
2014-11-22 01:23:38 +0800

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,14 @@ class ApplicationController < ActionController::Base
   def current_weichat_id
     session[:weichat_id]
   end
+  
+  def after_sign_in_path_for(resource)
+    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
+  
 end
