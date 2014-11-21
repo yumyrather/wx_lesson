c2b2ff6fbccec9ad529dee38858fa3146e45f951
Akira Panda <p.chenliang@gmail.com>
2014-11-22 01:29:27 +0800

    change login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
     if request.referer == sign_in_url
       super
     else
-      stored_location_for(resource) || request.referer || root_path
+      stored_location_for(resource) || request.referer || admin_root_path
     end
   end
   
